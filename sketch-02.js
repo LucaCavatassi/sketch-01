40732f07d96d66adc9fb2d27f542133183efff27
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-26 11:46:55 +0200

    feat: rotation

--- sketch-02.js
+++ sketch-02.js
@@ -1,13 +1,26 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1080, 1080 ]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+    
+    context.fillStyle = 'black';
+
+    const x = width * 0.5;
+    const y = height * 0.5;
+    const w = width * 0.3;
+    const h = height * 0.3;
+
+    context.rotate(0.3); // Corrected the typo
+
+    context.beginPath();
+    context.rect(x, y, w, h); // Changed to draw the rectangle centered
+    context.fill();
   };
 };
 
